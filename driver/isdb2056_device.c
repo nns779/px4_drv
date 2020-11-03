d91fc308620173a2f23388e648c18503db068ad2
nns779 <25547272+nns779@users.noreply.github.com>
2020-11-03 18:39:22 +0900

    driver: isdb2056_device: ptx_chrdevのオプションを追加

--- driver/isdb2056_device.c
+++ driver/isdb2056_device.c
@@ -967,7 +967,7 @@ int isdb2056_device_init(struct isdb2056_device *isdb2056, struct device *dev,
 		goto fail_device;
 
 	chrdev_config.ops = &isdb2056_chrdev_ops;
-	chrdev_config.options = 0;
+	chrdev_config.options = PTX_CHRDEV_WAIT_AFTER_LOCK_TC_T;
 	chrdev_config.ringbuf_size = 188 * px4_device_params.tsdev_max_packets;
 	chrdev_config.priv = &isdb2056->chrdev2056;
 
